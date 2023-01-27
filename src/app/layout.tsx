20128824f1f28c1acc86f9df4a923909826db7ec
Mai Veasna <veasna@go24.sg>
2023-01-27 17:01:01 +0700

    Added: Installed react icons and config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
-import './globals.css'
+import Header from "@/components/Header";
+import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { HiOutlineMenuAlt2 } from "react-icons/hi";
+
+export default function Header() {
+  return (
+    <div className=" max-w-6xl w-full m-auto s">
+      <HiOutlineMenuAlt2 size={30} />
+    </div>
+  );
+}
