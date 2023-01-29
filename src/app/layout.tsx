5fef4acdcf3111c594deb5a1ff50c953a673459e
Mai Veasna Dev <veasna@go24.sg>
2023-01-29 11:28:30 +0700

    Complated layout container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,24 @@
-import './globals.css'
- import prisma from "../lib/prisma";
+import Footer from "@/components/footer/Footer";
+import HeaderDesktop from "@/components/header/HeaderDesktop";
+import "./globals.css";
 
- export default async function RootLayout({
-   children,
- }: {
-   children: React.ReactNode;
- }) {
-   const user = await prisma.user.findMany();
-   console.log("pppp", user);
-
-   return (
-     <html lang="en">
-       <head />
-       <body>{children}</body>
-     </html>
-   );
- }
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head />
+      <body className="w-full flex flex-col space-y-2">
+        <HeaderDesktop />
+        <main className="max-w-6xl m-auto w-full h-[88vh] overflow-y-scroll flex justify-start flex-col items-start">
+          {children}
+        </main>
+        <div className=" fixed w-full bottom-0">
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
+}
